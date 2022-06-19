56e5952ca3ac61aedcb84434fc81eb926696c200
Ezh+Wine <valvic90@gmail.com>
2022-06-19 13:37:00 +0300

    task 3 completed

---
+++ Task3.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Clear();
+
+void ListSqrt(int number){
+    int count = 1;
+
+    Console.Write(number + " -> ");
+
+    if(number>0){
+
+    while (count<=number){
+        Console.Write(Math.Pow(count, 3) + ", ");
+        count++;
+    }}
+    else{
+        count = -1;
+        while (count>=number){
+        Console.Write(Math.Pow(count, 3) + ", ");
+        count--;
+    }
+}}
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ListSqrt(num);
